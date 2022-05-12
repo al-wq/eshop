3d7930ca993213eaea1f85e5a0b46da98f16d253
过眼烟云 <email@sample.com>
2022-05-12 22:23:18 +0800

    Initial commit

---
+++ eshop-login9100/src/main/java/com/demo/Login9100.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Login9100 {
+    public static void main(String[] args) {
+        SpringApplication.run(Login9100.class,args);
+    }
+}
