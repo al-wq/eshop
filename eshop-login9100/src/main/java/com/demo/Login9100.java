974e6845beab0cf90a4c7a1d00013861926d6c62
过眼烟云 <email@sample.com>
2022-05-12 23:02:41 +0800

    新增Swagger基础配置

--- eshop-login9100/src/main/java/com/demo/Login9100.java
+++ eshop-login9100/src/main/java/com/demo/Login9100.java
@@ -2,8 +2,10 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Login9100 {
     public static void main(String[] args) {
         SpringApplication.run(Login9100.class,args);
---
+++ eshop-login9100/src/main/java/com/demo/config/swagger/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.demo.config.swagger;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger配置类
+ * @description 配置Swagger信息
+ * @data 2022-05-12
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+}
